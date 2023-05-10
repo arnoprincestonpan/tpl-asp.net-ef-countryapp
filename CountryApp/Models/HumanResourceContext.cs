fbba9179ae146a98234674d1285f0bf49f46ea1e
panarno <78936893+panarno@users.noreply.github.com>
2023-05-09 17:26:13 -0700

    first commit

---
+++ CountryApp/Models/HumanResourceContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CountryApp.Models;
+
+public partial class HumanResourceContext : DbContext
+{
+    public HumanResourceContext()
+    {
+    }
+
+    public HumanResourceContext(DbContextOptions<HumanResourceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_dbo.Country");
+
+            entity.ToTable("Country");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Capital).HasMaxLength(50);
+            entity.Property(e => e.Currency).HasMaxLength(20);
+            entity.Property(e => e.Economy).HasColumnType("numeric(18, 0)");
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
